81c17666366f6cbc1786740a7f897dbf4b6771a5
Israel Ogunmola <israelogunmola@gmail.com>
2024-05-04 00:43:33 +0100

    feat: Add copy feature to HashSet

--- hashset.rb
+++ hashset.rb
@@ -18,6 +18,37 @@ class HashSet < HashMap
     @num_of_entries += 1
   end
 
+  def copy_entries_to(new_buckets)
+    # Copies over entries from older HashSet to a new one
+    @buckets.each do |bucket|
+      current_node = bucket.head
+      while current_node
+        new_index = to_index(current_node.data)
+        new_buckets[new_index].append(current_node.data)
+        current_node = current_node.next
+      end
+    end
+    new_buckets
+  end
+
+  def pretty_print
+    @buckets.each_with_index do |bucket, index|
+      print "🪣 Bucket #{index}: "
+      if bucket.head.nil?
+        puts 'empty'
+      else
+        current_node = bucket.head
+        while current_node
+          print "#{current_node.data}"
+          current_node = current_node.next
+          print ' -> ' unless current_node.nil?
+        end
+        puts '' # New line after each bucket
+      end
+    end
+    puts "#{length} entries in total"
+  end
+
   def keys
     entries
   end
